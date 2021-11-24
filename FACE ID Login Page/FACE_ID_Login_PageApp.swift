c7f7c8cafb31bf07ed82e7abdb9a0f3e80f0c8b0
Robertto Flores <roberttozappiain@gmail.com>
2021-11-24 06:31:33 -0800

    Initial Commit

---
+++ FACE ID Login Page/FACE_ID_Login_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FACE_ID_Login_PageApp.swift
+//  FACE ID Login Page
+//
+//  Created by Robertto Flores on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FACE_ID_Login_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
